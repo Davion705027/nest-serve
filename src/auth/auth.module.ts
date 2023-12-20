4ca14f1c897480decbd35b6a3fa5f770e01c24fd
davion <pmtydavion>
2023-12-20 19:30:14 +0800

    获取token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { jwtConstants } from './constants'; 
+import { JwtModule } from '@nestjs/jwt';
 @Module({
+  imports:[
+    JwtModule.register({
+      global:true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '10000000000000000'
+    }
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService]
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+    constructor( private jwtService : JwtService){}
     auth(){
         console.log('atuh');
         return 'auth auth 被user掉用';
+    }
+
+    async login(name?,pass?){
+        const payload = { sub: 12345, name:'davion' }
+        const token = await this.jwtService.signAsync(payload)
+        return token
     }
 }
