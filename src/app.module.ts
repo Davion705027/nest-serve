9b519d42590e2c7d2c9297ea7ccf30e9bbac70a9
davion <pmtydavion>
2023-12-30 16:43:40 +0800

    动态引入实体

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth/auth.guard';
+import { ProjectModule } from './project/project.module';
+// import { User } from './user/entities/user.entity';
+// import { Project } from './project/entities/project.entity';
+// console.log(__dirname); // /Users/admin/Desktop/code/personal/nest-serve/dist
+// console.log();
 
 @Module({
   imports: [
@@ -17,11 +21,16 @@ import { AuthGuard } from './auth/auth.guard';
       username: 'root',
       password: '12345678',
       database: 'nest-test',
-      entities: [User],
+      entities: [
+        __dirname + '/**/*.entity{.ts,.js}',
+        // User,Project
+      ],// 用于指定在整个应用程序中的数据库表映射实体。这是应用程序的根模块配置，
+                              // 用于创建数据库连接并定义整个应用程序中的所有实体
       synchronize: true,
     }),
     UserModule,
-    AuthModule],
+    AuthModule,
+    ProjectModule],
   controllers: [AppController],
   providers: [
     {
